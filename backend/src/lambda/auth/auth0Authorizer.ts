2ff7a435de7b9433adf522af30771be7078aadbd
Lawrence9908 <mugwenalawrence121@gmail.com>
2023-03-12 20:36:07 +0200

    Auth implementation completed

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -55,6 +55,7 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
+  logger.info("Verify token");
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
